d3a6928487048acac41d6c8ca7a95ac70d835b65
emkael <emkael@tlen.pl>
2016-05-23 13:52:45 +0200

    * initial, empty project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BCDD
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}

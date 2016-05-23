71fc38dd129db3a43e0bb6c894aaea1d14e17286
emkael <emkael@tlen.pl>
2016-05-23 14:50:52 +0200

    * opening PBN file list

---
+++ PBNFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace BCDD
+{
+    class PBNFile
+    {
+        private String filename;
+
+        public PBNFile(String filename)
+        {
+            this.filename = filename;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 namespace BCDD
 {
@@ -9,6 +10,18 @@ namespace BCDD
         [STAThread]
         static void Main(string[] args)
         {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Multiselect = true;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String filename in fd.FileNames)
+                {
+                    Console.WriteLine("Analyzing " + filename);
+                    PBNFile file = new PBNFile(filename);
+                }
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+            }
         }
     }
 }

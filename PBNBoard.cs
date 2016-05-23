22ff6c7e6ca9f697e97a1b1b43c461821286e63f
emkael <emkael@tlen.pl>
2016-05-23 14:56:43 +0200

    * PBN board/field structures with file operation mechanisms

---
+++ PBNBoard.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BCDD
+{
+    class PBNField
+    {
+        public String Key;
+        public String Value;
+        public String RawField;
+
+        public PBNField() { }
+        
+        public PBNField(String key, String value)
+        {
+            this.Key = key;
+            this.Value = value;
+            this.RawField = String.Format("[{0} \"{1}\"]", this.Key, this.Value);
+        }
+
+        public PBNField(String rawData)
+        {
+            this.RawField = rawData;
+        }
+    }
+
+    class FieldNotFoundException : Exception
+    {
+        public FieldNotFoundException() : base() { }
+        public FieldNotFoundException(String msg) : base(msg) { }
+    }
+
+    class PBNBoard
+    {
+        public List<PBNField> Fields;
+
+        public PBNBoard(List<string> lines)
+        {
+            this.Fields = new List<PBNField>();
+            Regex linePattern = new Regex(@"\[(.*) ""(.*)""\]");
+            foreach (String line in lines)
+            {
+                PBNField field = new PBNField();
+                field.RawField = line;
+                Match lineParse = linePattern.Match(line);
+                if (lineParse.Success)
+                {
+                    field.Key = lineParse.Groups[1].Value;
+                    field.Value = lineParse.Groups[2].Value;
+                }
+                this.Fields.Add(field);
+            }
+        }
+
+        public bool HasField(String key)
+        {
+            foreach (PBNField field in this.Fields)
+            {
+                if (key.Equals(field.Key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public String GetField(String key)
+        {
+            foreach (PBNField field in this.Fields)
+            {
+                if (key.Equals(field.Key))
+                {
+                    return field.Value;
+                }
+            }
+            throw new FieldNotFoundException(key + " field not found");
+        }
+
+        public void DeleteField(String key)
+        {
+            List<PBNField> toRemove = new List<PBNField>();
+            foreach (PBNField field in this.Fields)
+            {
+                if (key.Equals(field.Key))
+                {
+                    toRemove.Add(field);
+                }
+            }
+            foreach (PBNField remove in toRemove)
+            {
+                this.Fields.Remove(remove);
+            }
+        }
+
+    }
+}
--- PBNFile.cs
+++ PBNFile.cs
@@ -8,12 +8,60 @@ namespace BCDD
 {
     class PBNFile
     {
+        public List<PBNBoard> Boards;
+
         private String filename;
+        private String tmpFileName;
+
+        StreamWriter outputFile;
 
         public PBNFile(String filename)
         {
             this.filename = filename;
+            this.Boards = new List<PBNBoard>();
+            String[] contents = File.ReadAllLines(this.filename).Select(l => l.Trim()).ToArray();
+            List<String> lines = new List<String>();
+            foreach (String line in contents)
+            {
+                if (line.Length == 0)
+                {
+                    if (lines.Count > 0)
+                    {
+                        this.Boards.Add(new PBNBoard(lines));
+                        lines = new List<String>();
+                    }
+                }
+                else
+                {
+                    lines.Add(line);
+                }
+            }
+            if (lines.Count > 0)
+            {
+                this.Boards.Add(new PBNBoard(lines));
+            }
         }
 
+        public void WriteBoard(PBNBoard board)
+        {
+            if (this.outputFile == null)
+            {
+                this.tmpFileName = Path.GetTempFileName();
+                this.outputFile = new StreamWriter(new FileStream(this.tmpFileName, FileMode.Create), Encoding.UTF8);
+            }
+            foreach (PBNField field in board.Fields)
+            {
+                this.outputFile.WriteLine(field.RawField);
+            }
+            this.outputFile.WriteLine();
+        }
+
+        public void Save()
+        {
+            this.outputFile.Flush();
+            this.outputFile.Close();
+            File.Delete(this.filename);
+            File.Move(this.tmpFileName, this.filename);
+        }
     }
 }

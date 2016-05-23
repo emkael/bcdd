c21f4376d366d7a930bdbc850c659cd08e40e176
emkael <emkael@tlen.pl>
2016-05-23 22:06:28 +0200

    * reading file paths from command line arguments

--- Program.cs
+++ Program.cs
@@ -2,65 +2,83 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace BCDD
 {
     class Program
     {
+        static List<String> getFiles(string[] args)
+        {
+            List<String> filenames = new List<String>();
+            foreach (String arg in args)
+            {
+                if (File.Exists(arg))
+                {
+                    filenames.Add(arg);
+                }
+            }
+            if (filenames.Count == 0)
+            {
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.Multiselect = true;
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    filenames = new List<String>(fd.FileNames);
+                }
+            }
+            return filenames;
+        }
+
         [STAThread]
         static void Main(string[] args)
         {
-            OpenFileDialog fd = new OpenFileDialog();
-            fd.Multiselect = true;
-            if (fd.ShowDialog() == DialogResult.OK)
+            foreach (String filename in Program.getFiles(args))
             {
-                foreach (String filename in fd.FileNames)
+                Console.WriteLine("Analyzing " + filename);
+                PBNFile file = new PBNFile(filename);
+                foreach (PBNBoard board in file.Boards)
                 {
-                    Console.WriteLine("Analyzing " + filename);
-                    PBNFile file = new PBNFile(filename);
-                    foreach (PBNBoard board in file.Boards)
+                    DDTable table = new DDTable(board);
+                    String boardNo;
+                    try
                     {
-                        DDTable table = new DDTable(board);
-                        String boardNo;
-                        try
+                        boardNo = board.GetNumber();
+                    }
+                    catch (FieldNotFoundException)
+                    {
+                        boardNo = "?";
+                    }
+                    try
+                    {
+                        int[,] ddTable = table.GetDDTable();
+                        if (ddTable != null)
                         {
-                            boardNo = board.GetNumber();
+                            Console.WriteLine("Board " + boardNo);
+                            DDTable.PrintTable(ddTable);
+                            ParScore par = new ParScore(board);
+                            ParContract contract = par.GetParContract(ddTable);
+                            Console.WriteLine(contract);
+                            Console.WriteLine();
+                            board.SaveDDTable(ddTable);
+                            board.SaveParContract(contract);
+                            file.WriteBoard(board);
                         }
-                        catch (FieldNotFoundException)
+                        else
                         {
-                            boardNo = "?";
-                        }
-                        try
-                        {
-                            int[,] ddTable = table.GetDDTable();
-                            if (ddTable != null)
-                            {
-                                Console.WriteLine("Board " + boardNo);
-                                DDTable.PrintTable(ddTable);
-                                ParScore par = new ParScore(board);
-                                ParContract contract = par.GetParContract(ddTable);
-                                Console.WriteLine(contract);
-                                Console.WriteLine();
-                                board.SaveDDTable(ddTable);
-                                board.SaveParContract(contract);
-                                file.WriteBoard(board);
-                            }
-                            else
-                            {
-                                Console.WriteLine("ERROR: unable to determine DD table for board " + boardNo);
-                            }
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine(e.Message);
-                            Console.WriteLine(e.StackTrace);
+                            Console.WriteLine("ERROR: unable to determine DD table for board " + boardNo);
                         }
                     }
-                    file.Save();
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine(e.StackTrace);
+                    }
                 }
-                Console.WriteLine("Press any key to continue...");
-                Console.ReadLine();
+                file.Save();
             }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
         }
     }
 }
